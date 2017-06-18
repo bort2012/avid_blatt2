f892790dc90f4d135d5c4f04f96cc132ed363918
Nidzo <nikola.vidic@hotmail.de>
2017-06-18 14:12:53 +0200

    Methoden in RoarMgmBean implementiert

--- Roary-EJB-Beans/ejbModule/src/RoarMgmBean.java
+++ Roary-EJB-Beans/ejbModule/src/RoarMgmBean.java
@@ -1,11 +1,13 @@
 package src;
 
+import java.util.Date;
 import java.util.List;
 
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 import de.uniulm.avid.roary.RoarMgmBeanLocal;
 import de.uniulm.avid.roary.entities.Roar;
@@ -21,7 +23,7 @@ public class RoarMgmBean implements RoarMgmBeanLocal {
 
 	
 	@PersistenceContext(unitName="roarHistory")
-	EntityManager em;
+	EntityManager entityManager;
 	
     /**
      * Default constructor. 
@@ -32,32 +34,54 @@ public class RoarMgmBean implements RoarMgmBeanLocal {
 
 	@Override
 	public void addRoar(String auth, String txt, int feedId) throws FeedNotFoundException {
-		// TODO Auto-generated method stub
-		
+		Roar roar = new Roar();
+		roar.setAuthor(auth);
+		roar.setText(txt);
+		roar.setCreationDate(new Date());
+		entityManager.persist(roar);
 	}
 
 	@Override
 	public Roar getRoar(int roarId) throws RoarNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
+		Roar roar = entityManager.find(Roar.class, roarId);
+		if(roar != null){
+			return roar;
+		}else{
+			throw new RoarNotFoundException("Roar mit der id: " + roarId +" nicht gefunden!");
+		}
 	}
 
 	@Override
 	public void updateRoar(int roarId, String roarText) throws RoarNotFoundException {
 		// TODO Auto-generated method stub
-		
+		Roar roar = getRoar(roarId);
+		if(roar != null){
+			roar.setText(roarText);
+			roar.setCreationDate(new Date());
+			entityManager.merge(roar);
+		}else{
+			throw new RoarNotFoundException("Roar mit der id: " + roarId +" nicht gefunden!");
+		}
 	}
 
 	@Override
 	public void deleteRoar(int roarId) throws RoarNotFoundException {
 		// TODO Auto-generated method stub
-		
+		Roar roar = entityManager.find(Roar.class, roarId);
+		if(roar != null){
+			entityManager.remove(roar);
+		}else{
+			throw new RoarNotFoundException("Roar mit der id: " + roarId +" nicht gefunden!");
+		}
 	}
 
 	@Override
 	public List<Roar> getAllRoars(int feedId) throws FeedNotFoundException {
 		// TODO Auto-generated method stub
-		return null;
+		Query query = entityManager.createQuery("SELECT roars FROM roars");
+		List<Roar> roars = query.getResultList();
+		return roars;
+//		return null;
 	}
 
 }
