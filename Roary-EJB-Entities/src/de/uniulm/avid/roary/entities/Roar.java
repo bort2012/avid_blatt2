968817e820404cf33137727e7594db8f50272275
Nidzo <nikola.vidic@hotmail.de>
2017-06-18 18:26:35 +0200

    user Login in Roary-EJB-WebApp hinzugefügt

--- Roary-EJB-Entities/src/de/uniulm/avid/roary/entities/Roar.java
+++ Roary-EJB-Entities/src/de/uniulm/avid/roary/entities/Roar.java
@@ -2,16 +2,34 @@ package de.uniulm.avid.roary.entities;
 import java.io.Serializable;
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
+@Table(name="roars")
 public class Roar implements Serializable{
 	
 	static final long serialVersionUID = 1L;
 
+	@Id
+	@Column(name="roar_id", nullable=false, updatable=false)
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	protected int id;
+	
+	@Column(name="roar_text", nullable=false)
 	protected String text;
+	
+	@Column(name="roar_author", nullable=false)
 	protected String author;
+	
+	@Column(name="roar_date", nullable=false, insertable=false)
+	@Temporal(TemporalType.TIMESTAMP)
 	protected Date  creationDate;
 	/**
 	 * @return the serialversionuid
